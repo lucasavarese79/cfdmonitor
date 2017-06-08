08b609c24788e5b43ef0fe8d607b39f4f891a483
luca.savarese <lucasavarese79@gmail.com>
2017-06-08 18:01:36 +0200

    foreach

--- api/controllers/StartupController.js
+++ api/controllers/StartupController.js
@@ -10,43 +10,106 @@ var cfdController = require('./CfdController');
 var request = require("request"),
   cheerio = require("cheerio"),
   urls = ["http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888, "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02881];
-  urls=["http://www.nasdaq.com/symbol/aapl/analyst-research","http://www.nasdaq.com/symbol/sbux/analyst-research"];	  
+  urls=["DDD",
+"AIG",
+"BABA",
+"GOOGLE",
+"MO",
+"AMZN",
+"AXP",
+"AMGN"]
+/*,
+"AAPL",
+"T",
+"BIDU",
+"BAC",
+"BBRY",
+"BA",
+"CAT",
+"CSCO",
+"C",
+"KO",
+"EBAY",
+"FB",
+"RACEUS",
+"FSLR",
+"F",
+"GE",
+"GM",
+"GILD",
+"GRPN",
+"IBM",
+"INTC",
+"JNJ",
+"JPM",
+"MCD",
+"MU",
+"MSFT",
+"NFLX",
+"NEM",
+"NOK",
+"NVDA",
+"ORCL",
+"PYPL",
+"PEP",
+"PFE",
+"PG",
+"SHLD",
+"SIRI",
+"SNAP",
+"SBUX",
+"TSLA",
+"TWTR",
+"DIS",
+"YHOO",
+"ZNGA"]
+*/
+;
+
+
+  var prefix="http://www.nasdaq.com/symbol/";
+  var suffix="/analyst-research";
+
 var temp;
 
 module.exports = {
 
 	call: function (req, res){
 
-		for (i=0;i<urls.length;i++)
-	    request(urls[i], function (error, response, body) {
-		  if (!error) {
-		    var $ = cheerio.load(body),
-		      temp = $("[data-variable='temperature'] .wx-value").html();
-			  
-			  temp= $(".qwidget-dollar").html();
-			  temp2=$(".half-width-wide").html();
-			  temp3=$.html();
-			  start_cons=temp3.substring(temp3.indexOf("Consensus</td>"));
-			  end_cons=start_cons.substring(0,start_cons.indexOf("</table>"));
-			  start_block=end_cons.substring(end_cons.indexOf("<tr>"));
-			  end_block=start_block.substring(0,start_block.indexOf("</tr>"));
-			  end_block=end_block.replace(new RegExp('<td align="left"></td>', 'g'), '');
-			  
-			  var cons=end_block.substring(end_block.indexOf('left">')+6,end_block.indexOf("</td"));
-			  
-			  Cfd.create({name:temp, consensus:cons}).exec(function (err, finn){
-				  if (err) { return res.serverError(err); }
+		urls.forEach ((_cfd, i)=>{
+			var url_temp=prefix+_cfd+suffix;
+			sails.log("URL:" + url_temp);
+				  
+		    request(url_temp,function (error, response, body) {
+			  if (!error) {
+			    var $ = cheerio.load(body),
+			      temp = $("[data-variable='temperature'] .wx-value").html();
+				  temp= $(".qwidget-dollar").html();
+				  temp2=$(".half-width-wide").html();
+				  temp3=$.html();
+				  start_cons=temp3.substring(temp3.indexOf("Consensus</td>"));
+				  end_cons=start_cons.substring(0,start_cons.indexOf("</table>"));
+				  start_block=end_cons.substring(end_cons.indexOf("<tr>"));
+				  end_block=start_block.substring(0,start_block.indexOf("</tr>"));
+				  end_block=end_block.replace(new RegExp('<td align="left"></td>', 'g'), '');
+				  
+				  var cons=end_block.substring(end_block.indexOf('left">')+6,end_block.indexOf("</td"));
+				  sails.log("URL:" + url_temp+" "+temp+" "+cons);
+				  
+				  Cfd.create({name:temp, consensus:cons}).exec(function (err, finn){
+					  if (err) { return res.serverError(err); }
 
-				  sails.log('Finn\'s id is:', finn.id);
-				  //return res.ok();
-				});	
-		    	      
-		    console.log("It’s " + temp + " degrees Fahrenheit.");
-		  } else {
-		    console.log("We’ve encountered an error: " + error);
-		  }
+					  sails.log('Finn\'s id is:', finn.id);
+					  //return res.ok();
+					});	
+			    	      
+			    console.log("It’s " + temp + " degrees Fahrenheit.");
+			  } else {
+			    console.log("We’ve encountered an error: " + error);
+			  }
+			});
+			
 		});
-
 		return res.json({
 		    todo: 'Not implemented yet!'
 		 });
